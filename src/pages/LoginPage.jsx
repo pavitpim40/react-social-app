4c4f62a8ba3178cfe5fd251923faeed569145fd5
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-07 15:33:09 +0700

    feat:login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import { Box, Typography, Button, TextField, Divider } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 // import Box from '@mui/material/Box'
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const [emailOrPhone, setEmailOrPhone] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmailOrPhone = (e) => setEmailOrPhone(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
+  const handleSubmitLogin = (e) => {
+    e.preventDefault();
+    if (emailOrPhone === 'qwer' && password === '1234') {
+      navigate('/');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
   return (
     <Box component='main' height='100vh' display='flex' justifyContent='space-between'>
       <Box
@@ -36,9 +52,24 @@ function LoginPage() {
           padding='16px'
           backgroundColor='white'
           borderRadius={1}
+          onSubmit={handleSubmitLogin}
         >
-          <TextField type='text' size='small' variant='outlined' label='Email or Phone number' />
-          <TextField type='password' size='small' variant='outlined' label='password' />
+          <TextField
+            type='text'
+            size='small'
+            variant='outlined'
+            label='Email or Phone number'
+            value={emailOrPhone}
+            onChange={handleChangeEmailOrPhone}
+          />
+          <TextField
+            type='password'
+            size='small'
+            variant='outlined'
+            label='password'
+            value={password}
+            onChange={handleChangePassword}
+          />
           <Button type='submit' variant='contained' disableElevation color='primary'>
             Login
           </Button>
